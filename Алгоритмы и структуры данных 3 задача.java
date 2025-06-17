00ca3da774e14a6b21c801a8ea3ea6d90a87d741
Vlad9nich <vladguy228@gmail.com>
2025-06-17 21:49:51 +0300

    Create Алгоритмы и структуры данных 3 задача.java

---
+++ Алгоритмы и структуры данных 3 задача.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class BestTravel {
+    public static void main(String[] args) {
+        int[] distances = {50, 55, 57, 58, 60};
+        int maxDistance = 175;
+        int bestSum = findBestSum(distances, 3, maxDistance);
+        
+        System.out.println("Лучшая сумма расстояний: " + bestSum);
+    }
+    
+    static int findBestSum(int[] distances, int citiesToVisit, int maxDistance) {
+        int bestSum = -1;
+        
+        // Перебор всех комбинаций из 3 городов
+        for (int i = 0; i < distances.length; i++) {
+            for (int j = i + 1; j < distances.length; j++) {
+                for (int k = j + 1; k < distances.length; k++) {
+                    int currentSum = distances[i] + distances[j] + distances[k];
+                    
+                    if (currentSum == maxDistance) {
+                        return maxDistance; 
+                    }
+                    
+                    if (currentSum < maxDistance && currentSum > bestSum) {
+                        bestSum = currentSum;
+                    }
+                }
+            }
+        }
+        
+        return bestSum;
+    }
+}
